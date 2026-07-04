ef962ec6d3891cdf2b8d8898874bfbb4fc2e767e
ouaaitalla <houssamaitalla5@gmail.com>
2026-07-04 12:20:31 +0100

    struct post

--- backend/models/models.go
+++ backend/models/models.go
@@ -1,11 +1,19 @@
 package models
 
-type registerReq struct{
-	Username string `json:"username"`
-	Email string `json:"email"`
-	Password string `json:"passowrd"`
+type Post struct {
+	ID             string `json:"id"`
+	UserID         string `json:"user_id"`
+	AuthorNickname string `json:"author_nickname"`
+	Title          string `json:"title"`
+	Content        string `json:"content"`
+	Category       string `json:"category"`
+	CreatedAt      string `json:"created_at"`
+	CommentCount   int    `json:"comment_count"`
 }
-type loginReq struct{
-	Username string `json:"username"`
-	Password string `json:"passowrd"`
+
+type PostRequest struct {
+	Title    string `json:"title"`
+	Content  string `json:"content"`
+	Category string `json:"category"`
 }
+
