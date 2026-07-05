9f8eae492cf9be7601b1c84eac396fdfde30b451
aelhassn <hassnaouiabdelali89@gmail.com>
2026-07-05 22:18:56 +0100

    build the login page and scc for it

---
+++ frontend/js/pages/login.js
@@ -0,0 +1,36 @@
+export function loginTemplate() {
+    return `
+        <div class="login-container">
+            <h1>Login</h1>
+
+            <form id="login-form">
+
+                <label for="identifier">Email or Nickname</label>
+                <input
+                    type="text"
+                    id="identifier"
+                    name="identifier"
+                    placeholder="Enter your email or nickname"
+                    required
+                >
+
+                <label for="password">Password</label>
+                <input
+                    type="password"
+                    id="password"
+                    name="password"
+                    placeholder="Enter your password"
+                    required
+                >
+
+                <button type="submit">Login</button>
+
+            </form>
+
+            <p>
+                Don't have an account?
+                <a href="#" id="go-register">Register</a>
+            </p>
+        </div>
+    `;
+}
